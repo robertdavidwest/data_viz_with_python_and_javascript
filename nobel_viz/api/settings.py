5a0fcc01f1c35024972ecc4e442b89919bae95c8
Robert <robert.david.west@gmail.com>
2016-10-31 21:50:07 -0400

    data server ready for front end

--- nobel_viz/api/settings.py
+++ nobel_viz/api/settings.py
@@ -8,14 +8,20 @@
 
 URL_PREFIX =  'api'
 MONGO_DBNAME = 'nobel_prize'
-DOMAIN = {'winners_clean': {
+DOMAIN = {'winners_all_clean': {
+    'item_title': 'winners',
     'schema': {
         'country': {'type': 'string'},
         'category': {'type': 'string'},
         'name': {'type': 'string'},
         'year': {'type': 'integer'},
-        'gender': {'type': 'string'}
-    }
+        'gender': {'type': 'string'},
+        'mini_bio': {'type': 'string'},
+        'bio_image': {'type': 'string'}
+    },
+    'url': 'winners'
 }}
 
 X_DOMAINS = 'http://127.0.0.1:8080'
+HATEOAS = False
+PAGINATION = False
--- nobel_viz/static/js/script.js
+++ nobel_viz/static/js/script.js
@@ -1,21 +1,31 @@
 var API_URL = 'http://127.0.0.1:5000/api';
 
-
 var displayJSON = function(query){
     d3.json(API_URL + query, function(error, data){
 
         // log any error to the console as a warning
-        if (error){
+        if (error){    
             console.warn(error);
         }
 
         d3.select("#query pre").html(query);
         d3.select("#data pre").html(JSON.stringify(data, null, 4));
+        
         console.log(data);
     });
 };
 
-var query = '/winners_clean?where=' + JSON.stringify({
+var query = '/winners?where=' + JSON.stringify({
     "year": {"$gt": 2000},
     "gender": "female"
 });
+
+var query = '/winners?projection=' + JSON.stringify({
+    "mini_bio": 0
+});
+
+var query = '/winners?where=' + JSON.stringify({
+    "name": "Albert Einstein"
+});
+
+displayJSON(query);
